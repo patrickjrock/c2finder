6d21560c2cabf158849004f9184c87a2e2ab0302
raafae753 <syedismail@Syeds-MacBook-Pro.local>
2016-11-17 15:24:41 -0600

    Modified loop

--- hmm_graph.py
+++ hmm_graph.py
@@ -34,7 +34,7 @@ class HMM_graph:
         self.G.add_node(i, e=self.uniform_distribution()) # using uniform distribution for now
     for i in node_range[:-1]:
         self.G.add_edge(i, i+1)
-        for k in node_range[:-1]:
+        for k in node_range[2:]:
             self.G.add_edge(i, k)
 
   def add_loop(self, n):
